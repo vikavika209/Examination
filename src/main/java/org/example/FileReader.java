70b4de584d1976c3cd306c05cd22215adaf8ae6b
vikavika209@gmail.com <vikavika209@gmail.com>
2024-11-17 19:13:12 +0500

    Initial commit

---
+++ src/main/java/org/example/FileReader.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+public class Keys {
+
+    public Map<Integer, String> getKeysMapFromFile(Path fhilePath){
+        Files.lines(fhilePath)
+                .spliterator("-")
+
+
+
+        return
+    }
+
+}
---
+++ src/main/java/org/example/ResultsProcessor.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Map;
+
+public class FilesComparator {
+    Map<Integer, String> mapKey;
+    Map<Integer, String> mapAnswers;
+
+    public int result(Map<Integer, String> map1, Map<Integer, String> map2){
+        
+    }
+}
